ff4452723bb7c4d3d95b9cc8521c4ab9534b8959
victors <victordaniel64x@gmail.com>
2024-03-27 12:00:46 -0400

    swagger

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,14 +41,22 @@ INSTALLED_APPS = [
     'todos.apps.TodosConfig',
     # 3rd party
     'rest_framework',
-    'corsheader',
+    'corsheaders',
+    'drf_spectacular',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Todos API',
+    'DESCRIPTION': 'Un API de una lista de tareas',
+    'VERSION': '1.0.0',
 }
 
 MIDDLEWARE = [
